ee2b56de353f04288e8fd05a44228ab5c55964f0
nikachikviladze <nikachikviladze93@gmail.com>
2022-05-21 15:33:02 +0400

    cache and observer

---
+++ app/Observers/BlogObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Blog;
+use Illuminate\Support\Facades\Cache;
+
+class BlogObserver
+{
+    /**
+     * Handle the Blog "created" event.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return void
+     */
+    public function created(Blog $blog)
+    {
+        Cache::forget('blogs');
+    }
+
+    /**
+     * Handle the Blog "updated" event.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return void
+     */
+    public function updated(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "deleted" event.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return void
+     */
+    public function deleted(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "restored" event.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return void
+     */
+    public function restored(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "force deleted" event.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return void
+     */
+    public function forceDeleted(Blog $blog)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PagesController;
-
-
+use App\Models\Blog;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/',  [PagesController::class, 'index']);
 Route::get('/download',  [PagesController::class, 'download']);
@@ -12,3 +12,15 @@ Route::post('/upload',  [PagesController::class, 'upload']);
 
 Route::get('/files',  [PagesController::class, 'files'])->name('files');
 Route::delete('/files/{name}',  [PagesController::class, 'destroy_file'])->name('file_delete');
+
+Route::get('blog', function(){
+
+    $blogs = Cache::remember('blogs', 60*60*24, function () {
+        return Blog::all();
+    });
+
+    
+
+
+    dd($blogs);
+});
